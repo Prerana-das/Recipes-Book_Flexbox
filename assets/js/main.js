6e5136e6410fbcea58030c7a022adbdcc35f3aad
prerana das <preranadas97@gmail.com>
2019-03-14 23:52:42 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+!function(e){
+	"use strict";
+
+// Documnet ready function 
+// Implement your function in it
+$(document).ready(function(){
+	
+});
+
+
+
+
+//Window load function 
+$(window).load(function(){
+	
+});
